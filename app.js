722417de819f652708c38db6b4165b17c33045b6
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-08-15 17:33:30 +0300

    hw2

--- app.js
+++ app.js
@@ -64,29 +64,29 @@ app.delete('/users/:userId', async (req, res) => {
         return;
     }
 
-    res.json('OK');
     res.sendStatus(204);
 });
 
+app.put('/users/:userId', async (req, res) => {
+    const { userId } = req.params;
+    // const { age, name } = req.body;
 
-// app.delete('/users/:userId', (req, res) => {
-//     // const userId = req.params.user;
-//     const {userId} = req.body;
-//     const account = users[userId];
-//
-//     if (!account) {
-//         return res.status(400).json('User not found');
-//     }
-//
-//     delete users[userId];
-//     return res.status(204)
-//
-// });
-//
+    if (Number.isNaN(+userId) || +userId < 0) {
+        res.status(400).json('Wrong user id');
+        return;
+    }
 
+    const user = await fileService.updateUser(+userId, req.body);
 
+    if (!user) {
+        res.status(404).json('User not found');
+        return;
+    }
 
 
+    res.status(201).json(user);
+
+});
 
 
 app.listen(3000, () =>{
@@ -98,103 +98,3 @@ app.listen(3000, () =>{
 
 
 
-
-//
-// const express = require('express');
-// const users = require('./dataBase');
-//
-// const fileService = require('./services/file.service');
-//
-// const app = express();
-// @@ -14,19 +14,31 @@ app.get('/', (req, res) => {
-// });
-//
-// app.get('/users', async (req, res) => {
-//     let usersFronService = await fileService.getUsers();
-//     const usersFronService = await fileService.getUsers();
-//     res.json(usersFronService);
-// });
-// app.post('/users', async (req, res) => {
-//     const { age, name } = req.body;
-//
-//     if (Number.isNaN(+age) || age <= 0) {
-//         res.status(400).json('Wrong user age');
-//         return;
-//     }
-//
-//     const user = await fileService.insertUser({ age, name });
-//
-//     res.status(201).json(user);
-// });
-//
-// app.get('/users/:userId', (req, res) => {
-//     app.get('/users/:userId', async (req, res) => {
-//         const { userId } = req.params;
-//
-//         if (Number.isNaN(+userId) || +userId < 0) {
-//             res.status(400).json('Wrong user id');
-//             return;
-//         }
-//
-//         const user = users[userId];
-//         const user = await fileService.getOneUser(+userId);
-//
-//         if (!user) {
-//             res.status(404).json('User not found');
-//         @@ -36,23 +48,48 @@ app.get('/users/:userId', (req, res) => {
-//             res.json(user);
-//         });
-//
-//             app.post('/users', (req, res) => {
-//                 const {age, name} = req.body;
-//                 app.put('/users/:userId', async (req, res) => {
-//                     const { userId } = req.params;
-//                     const { age, name } = req.body;
-//
-//                     if (Number.isNaN(+userId) || +userId < 0) {
-//                         res.status(400).json('Wrong user id');
-//                         return;
-//                     }
-//
-//                     console.log(age, 'age');
-//                     console.log(name, 'name');
-//                     const userObject = {};
-//                     if (age) userObject.age = age;
-//                     if (name) userObject.name = name;
-//
-//                     if (Number.isNaN(+age) || age <= 0) {
-//                         res.status(400).json('Wrong user age');
-//                         const user = await fileService.updateUser(+userId, userObject);
-//
-//                         if (!user) {
-//                             res.status(404).json('User not found');
-//                             return;
-//                         }
-//
-//                         users.push({ name, age })
-//                         res.status(201).json(user);
-//                     });
-//
-//                     app.delete('/users/:userId', async (req, res) => {
-//                         const { userId } = req.params;
-//
-//                         if (Number.isNaN(+userId) || +userId < 0) {
-//                             res.status(400).json('Wrong user id');
-//                             return;
-//                         }
-//
-//                         const user = await fileService.deleteOneUser(+userId);
-//
-//                         if (!user) {
-//                             res.status(404).json('User not found');
-//                             return;
-//                         }
-//
-//                         res.json('OK');
-//                         res.sendStatus(204);
-//                     });
-//
-//                     app.listen(5000, () => {
-//                         console.log('App listen 5000')
-//                         console.log('App listen 5000');
-//                     });
--- services/file.service.js
+++ services/file.service.js
@@ -36,13 +36,11 @@ module.exports = {
         return reader();
 
     },
-
     getOneUser: async (id) => {
         const users = await reader();
 
         return users.find((user) => user.id === id);
     },
-
     insertUser: async (userObject) => {
         const users = await reader();
 
@@ -66,7 +64,19 @@ module.exports = {
 
             await writer(users);
             return user;
-        }
+        },
+    updateUser: async (id, data) => {
+        const users = await reader();
+
+        const index = users.findIndex((user) => user.id === id);
+
+        if (index < 0) return;
+
+        users[index] = { ...users[index], ...data };
+        await writer(users);
+
+        return users[index];
+    }
 }
 
 
@@ -75,90 +85,3 @@ module.exports = {
 
 
 
-
-
-
-
-
-
-
-// const usersDb = require('../dataBase')
-// const fs = require('fs/promises');
-// const path = require('path');
-//
-// const filePath = '../dataBase/users.json'
-// const pathToFile = path.join(process.cwd(), 'dataBase', 'users.json');
-//
-// const reader = async () => {
-//     try {
-//         const buffer = await fs.readFile(pathToFile);
-//         const data = buffer.toString();
-//
-//         const users = data ? JSON.parse(data) : [];
-//
-//         return users.sort((a, b) => a.id - b.id);
-//     } catch (e) {
-//         console.log(e);
-//     }
-// }
-//
-// const writer = async (users) => {
-//     try {
-//         await fs.writeFile(pathToFile, JSON.stringify(users));
-//     } catch (e) {
-//         console.log(e);
-//     }
-// }
-//
-// module.exports = {
-//     getUsers: () => {
-//         return reader();
-//     },
-//
-//     insertUser: async (userObject) => {
-//         // TODO fs.appendFile(filePath, userObject)
-//         const users = await reader();
-//
-//         userObject.id = users.length ? users[users.length - 1].id + 1 : 1;
-//         users.push(userObject);
-//
-//         await writer(users);
-//
-//         return userObject;
-//     },
-//
-//     getUsers: async () => {
-//         return usersDb;
-//         getOneUser: async (id) => {
-//             const users = await reader();
-//
-//             return users.find((user) => user.id === id);
-//         },
-//
-//             updateUser: async (id, data) => {
-//             const users = await reader();
-//
-//             const index = users.findIndex((user) => user.id === id);
-//
-//             if (index < 0) return;
-//
-//             users[index] = { ...users[index], ...data };
-//             await writer(users);
-//
-//             return users[index];
-//         },
-//
-//             deleteOneUser: async (id) => {
-//             const users = await reader();
-//
-//             const index = users.findIndex((user) => user.id === id);
-//
-//             if (index < 0) return;
-//
-//             const user = users[index];
-//             users.splice(index, 1);
-//
-//             await writer(users);
-//             return user;
-//         }
-//     }
