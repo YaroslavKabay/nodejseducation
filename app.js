55e0094bf5d35afc0f64c081ed3d92e706f37203
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-09-29 19:16:00 +0300

    hw11 lecture p1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config({path:`./environments/${process.env.NODE_ENV}.env`});
 
+const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
 const fileUpload = require('express-fileupload');
 
 const mongoose = require('mongoose');
@@ -11,6 +13,39 @@ const runCronJobs = require('./cron');
 const { mainErrorHandler } = require('./errors');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {cors: 'http://localhost:63342/'});
+// const io = socketIo(server, {cors: 'http://localhost:63342'});
+
+io.on('connection', (socket) => {
+    console.log('CONNECTED');
+    console.log('=========================================================');
+    console.log(socket);
+    console.log('=========================================================');
+
+    // console.log('=========================================================');
+    // console.log(socket.id);
+    // console.log(socket.handshake.auth);
+    // console.log('=========================================================');
+
+    // socket.on('message:create', (data) => {
+    //     console.log('data _________');
+    //     console.log(data);
+    //     console.log('data _________');
+    //
+    //     // EMIT EVENT TO SENDER
+    //     // socket.emit('user:create', { name: 'Socket', hard: 10 });
+    //
+    //     // EMIT EVENT ALL USERS INCLUDE SENDER
+    //     // io.emit('user:create', { name: 'Socket', hard: 10 });
+    //
+    //     // EMIT EVENT ALL USERS EXCLUDE SENDER
+    //     socket.broadcast.emit('user:create', { name: 'Socket', hard: 10 });
+    // });
+    //
+    // socket.on('room:join', (data) => userJoinRoom(io, socket, data));
+});
+
 
 if (NODE_ENV !== 'production') {
     const morgan = require('morgan');
@@ -37,7 +72,7 @@ app.use('*', (req, res, next)=>{
 
 app.use(mainErrorHandler);
 
-app.listen(PORT, () =>{
+server.listen(PORT, () =>{
     console.log('App listen', PORT);
     mongoose.connect(MONGO_URL);
     console.log(process.env.NODE_ENV);
